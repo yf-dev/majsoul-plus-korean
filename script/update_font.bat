1398fc652776890b55bd5347c705600b424897dd
Nesswit <rishubil@gmail.com>
2020-05-20 01:20:00 +0900

    Add spacing for font images

--- script/update_font.bat
+++ script/update_font.bat
@@ -7,12 +7,12 @@ if not defined MAJSOUL_LANG (
 rmdir src\generated\fonts /s /q
 mkdir src\generated\fonts
 if "%MAJSOUL_LANG%" == "jp" (
-  utils\fontbm\fontbm.exe --font-file fonts\NotoSansCJKkr-Medium.otf --output src\generated\fonts\jp_haolong --chars-file src\generated\chars.txt --data-format xml --max-texture-count=1 --texture-width=1536 --texture-height=2096 --font-size=40
-  utils\fontbm\fontbm.exe --font-file fonts\NotoSansCJKkr-Medium.otf --output src\generated\fonts\jp_jiye --chars-file src\generated\chars.txt --data-format xml --max-texture-count=1 --texture-width=1024 --texture-height=2096 --font-size=35
+  utils\fontbm\fontbm.exe --font-file fonts\NotoSansCJKkr-Medium.otf --output src\generated\fonts\jp_haolong --chars-file src\generated\chars.txt --data-format xml --spacing-vert=1 --spacing-horiz=1 --max-texture-count=1 --texture-width=1536 --texture-height=2096 --font-size=40
+  utils\fontbm\fontbm.exe --font-file fonts\NotoSansCJKkr-Medium.otf --output src\generated\fonts\jp_jiye --chars-file src\generated\chars.txt --data-format xml --spacing-vert=1 --spacing-horiz=1 --max-texture-count=1 --texture-width=1024 --texture-height=2096 --font-size=35
 ) else (
-  utils\fontbm\fontbm.exe --font-file fonts\SSRockRegular.ttf --output src\generated\fonts\en_hanyi --chars-file src\generated\chars.txt --data-format xml --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=40
-  utils\fontbm\fontbm.exe --font-file fonts\BMEULJIROTTF.ttf --output src\generated\fonts\en_haolong --chars-file src\generated\chars.txt --data-format xml --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=35
-  utils\fontbm\fontbm.exe --font-file fonts\SDKukdetopokki-aLt.otf --output src\generated\fonts\en_shuhun --chars-file src\generated\chars.txt --data-format xml --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=35
+  utils\fontbm\fontbm.exe --font-file fonts\SSRockRegular.ttf --output src\generated\fonts\en_hanyi --chars-file src\generated\chars.txt --data-format xml --spacing-vert=1 --spacing-horiz=1 --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=40
+  utils\fontbm\fontbm.exe --font-file fonts\BMEULJIROTTF.ttf --output src\generated\fonts\en_haolong --chars-file src\generated\chars.txt --data-format xml --spacing-vert=1 --spacing-horiz=1 --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=35
+  utils\fontbm\fontbm.exe --font-file fonts\SDKukdetopokki-aLt.otf --output src\generated\fonts\en_shuhun --chars-file src\generated\chars.txt --data-format xml --spacing-vert=1 --spacing-horiz=1 --max-texture-count=1 --texture-width=512 --texture-height=1024 --font-size=35
 )
 bin\combine_bitmapfont.exe
 if "%MAJSOUL_LANG%" == "jp" (
