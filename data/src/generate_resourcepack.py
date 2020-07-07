0090f3051a04c8b44e590a974c6bfa54f6f421dd
Nesswit <rishubil@gmail.com>
2020-07-07 22:06:43 +0900

    Update version code

--- data/src/generate_resourcepack.py
+++ data/src/generate_resourcepack.py
@@ -11,8 +11,8 @@ def main(dist_path):
     log_normal('Generate resourcepack.json...', verbose)
     resourcepack_data = {
         'id': 'korean',
-        'version': f'2.{datetime.now().strftime("%Y%m%d.1%H%M%S")}',
-        'name': '한국어',
+        'version': f'3.0.{datetime.now().strftime("%Y%m%d%H%M%S")}',
+        'name': '한국어(글로벌 서버)',
         'author': 'Nesswit',
         'description': '인터페이스를 한국어로 표시합니다.',
         'preview': 'preview.png',
@@ -26,6 +26,8 @@ def main(dist_path):
     assets_path = Path(dist_path) / 'assets'
     for file_path in sorted(assets_path.glob('**/*')):
         if file_path.is_dir():
+            continue
+        if file_path.name == '.gitkeep':
             continue
         resourcepack_data['replace'].append(file_path.relative_to(assets_path).as_posix())
 
