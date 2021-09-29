ec0059d0a92de6d78d2e570cc2b40fd72cdac713
Nesswit <rishubil@gmail.com>
2021-09-29 20:00:53 +0900

    Skip unpacking spine.atlas

--- data/src/mpk/unpack_all_atlas.py
+++ data/src/mpk/unpack_all_atlas.py
@@ -8,6 +8,9 @@ from .unpack_atlas import main as unpack_atlas
 def main(original_assets_path):
     log_normal("Unpack all atlas files...")
     for atlas_unpack_path in sorted(Path(original_assets_path).glob("**/*.atlas")):
+        if atlas_unpack_path.stem == 'spine':
+            log_info(f"Skip Spine atlas {atlas_unpack_path}...", is_verbose=True)
+            continue
         unpack_atlas(str(atlas_unpack_path))
     log_info("Unpack complete")
 
